f66bb92ec90d4ab9aaa78c31822341c273295807
ZyyGen <npdat2004@gmail.com>
2025-05-15 15:03:48 +0700

    Bai3SpringMVC

---
+++ FormSpringMVC/src/main/java/com/example/demo/FormSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSpringMvcApplication.class, args);
+	}
+
+}
